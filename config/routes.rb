ed295dbb86228a959022be3682d6016f52171a7b
Jacques Marzin​ <jacques.marzin@free.fr>
2014-04-06 15:17:08 +0200

    dimanche 6 avril

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Disic::Application.routes.draw do
+  resources :projets
+
+  resources :produits
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/features/page_spec.rb
+++ spec/features/page_spec.rb
@@ -1,13 +1,20 @@
 require 'spec_helper'
 
 describe 'La page' do
+  before :each do
+    visit 'users/sign_in'
+  end
 
   it 'contient un header' do
-    visit '/users/sign_in'
     page.should have_selector('header#page_header')
   end
   it 'contient un footer' do
-    visit '/users/sign_in'
     page.should have_selector('footer#page_footer')
+  end
+  it 'contient une sidebar' do
+    page.should have_selector('section#sidebar')
+  end
+  it 'contient un menu' do
+    page.should have_selector('nav#main_nav')
   end
 end
