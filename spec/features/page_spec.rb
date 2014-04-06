019c6eb340e656c7972885792a6ee6116db08a33
Jacques Marzin​ <jacques.marzin@free.fr>
2014-04-06 20:20:06 +0200

    dimanche 6 avril soir

--- spec/features/page_spec.rb
+++ spec/features/page_spec.rb
@@ -8,6 +8,20 @@ describe 'La page' do
   it 'contient un header' do
     page.should have_selector('header#page_header')
   end
+  it "ne contient pas de sign_in si aucun utilisateur n'est connecté" do
+    page.should_not have_selector('nav#utility')
+  end
+  it "contient le sign_out si un utilisateur est connecté" do
+    click_on 'Sign up'
+    fill_in 'user_email', :with => 'user@example.com'
+    fill_in 'user_password', :with => 'password'
+    fill_in 'user_password_confirmation', :with => 'password'
+    click_on 'Sign up'
+    page.should have_selector('nav#utility')
+    within('nav#utility') do
+      page.should have_content('user@example.com')
+    end
+  end
   it 'contient un footer' do
     page.should have_selector('footer#page_footer')
   end
